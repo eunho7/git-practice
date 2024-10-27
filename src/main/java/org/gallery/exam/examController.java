1888ff57a3808fe46fc73d4f4d907eb4d580d3f0
Eunho <76768140sa@gmail.com>
2024-10-27 17:17:21 +0900

    first commit

---
+++ src/main/java/org/gallery/exam/examController.java
@@ -0,0 +1,14 @@
+package org.gallery.exam;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class examController {
+    @GetMapping("/")
+    public ResponseEntity exam(){
+        String str = "안녕하세요.";
+        return null;
+    }
+}
---
+++ src/test/java/org/gallery/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gallery.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
